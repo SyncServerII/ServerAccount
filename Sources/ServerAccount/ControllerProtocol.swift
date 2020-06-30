c700d965204a296c295b2844638033261dcf8bb0
Christopher Prince <chris@SpasticMuffin.biz>
2020-06-29 22:47:09 -0600

    Added protocol-- so accounts can support endpoints.

---
+++ Sources/ServerAccount/ControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ControllerProtocol.swift
+//  
+//
+//  Created by Christopher G Prince on 6/29/20.
+//
+
+import Foundation
+
+public protocol ControllerProtocol {
+    static func setup() -> Bool
+}
